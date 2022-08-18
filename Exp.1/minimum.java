d36eab49627eae4c70411c4e467961eb036e8377
RKHariharan <107878555+RKHariharan@users.noreply.github.com>
2022-08-18 16:00:18 +0530

    Add files via upload

---
+++ Exp.1/minimum.java
@@ -0,0 +1,12 @@
+
+package minimum;
+public class minimum {
+    public static void main(String[] args) {
+       int a=Integer.valueOf(args[0]);
+       int b=Integer.valueOf(args[1]);
+       if(a<b)
+           System.out.println(a+"is a minimum");
+       else
+            System.out.println(b+"is minimum");
+    } 
+}
