be86f2b54a17730e8605fb538b960c1b4348fec8
RKHariharan <107878555+RKHariharan@users.noreply.github.com>
2022-08-19 11:58:15 +0530

    Add files via upload

---
+++ Exp.1/Starpattern.java
@@ -0,0 +1,23 @@
+
+import java.util.Scanner;
+
+public class Starpattern {
+    public static void main(String[] args) {
+          Scanner in=new Scanner(System.in);
+       System.out.println("enter the no. of words:");
+       int n=in.nextInt();
+       for(int i=0;i<n;i++)
+	{
+		for(int j=2*(n-i);j>=0;j--)
+		{
+			System.out.print(" ");
+		}
+		for(int j=0;j<=i;j++)
+		{
+			System.out.print("* ");
+		}
+		System.out.println();
+	}
+   } 
+}
+    
