50ec1cfc81bcf019c100c687ae72a8984ad3b283
RKHariharan <107878555+RKHariharan@users.noreply.github.com>
2022-09-20 22:10:43 +0530

    Add files via upload

---
+++ Exp5/Program1/year1marks.java
@@ -0,0 +1,24 @@
+package Yeear1;
+public class year1marks {
+     public double sub1mark,sub2mark;
+    public year1marks(double a,double b)
+    {
+        sub1mark=a;
+        sub2mark=b;          
+    }
+    public String Sub1(double m1)
+    {
+        if(m1>=50)
+            return "PASS";
+        else
+            return "FAIL";
+    }
+    public String Sub2(double m1)
+    {
+        if(m1>=50)
+            return "PASS";
+        else
+            return "FAIL";
+    }
+    
+}
