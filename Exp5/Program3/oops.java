22f4e070adef6b5a9f59a3976c03531bc4e19430
RKHariharan <107878555+RKHariharan@users.noreply.github.com>
2022-09-20 22:42:53 +0530

    Add files via upload

---
+++ Exp5/Program3/oops.java
@@ -0,0 +1,14 @@
+package oops;
+public class oops {
+    public class oops1 {
+    protected void display()
+    {
+        System.out.println("protected property");
+    }
+    void display1()
+    {
+        System.out.println("default property");
+    }
+   
+}
+}
---
+++ Exp5/Program3/suboops.java
@@ -0,0 +1,24 @@
+package real;
+public class suboops{
+class suboops extends oops1
+{
+    public static void main(String[]args)
+    {
+        suboops obj = new suboops();
+        obj.display();
+        try{
+            obj.display1();
+           
+        }
+        catch(Exception e)
+        {
+            System.out.println("default property cannot be displayed");
+        }
+       
+}
+
+    private void display1() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+}
+}
