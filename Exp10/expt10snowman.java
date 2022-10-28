01ea70debf121bf956499672206e160469850d3f
RKHariharan <107878555+RKHariharan@users.noreply.github.com>
2022-10-28 22:33:04 +0530

    Add files via upload

---
+++ Exp10/expt10snowman.java
@@ -0,0 +1,15 @@
+
+package com.mycompany.exp10;
+
+import java.awt.PopupMenu;
+
+/**
+ *
+ * @author PC
+ */
+public class expt10snowman extends PopupMenu {
+
+    public expt10snowman() {
+    }
+    
+}
